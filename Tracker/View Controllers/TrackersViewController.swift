cad750252b9c5c8653151a5c15b78e0a78880802
dkmenshikov <dkmenshikov@gmail.com>
2024-05-11 17:44:04 +0300

    вырезан Storyboard

---
+++ Tracker/View Controllers/TrackersViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TrackersViewController.swift
+//  Tracker
+//
+//  Created by Dmitriy Menshikov on 11.05.24.
+//
+
+import UIKit
+
+final class TrackersViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .tRed
+    }
+    
+}
